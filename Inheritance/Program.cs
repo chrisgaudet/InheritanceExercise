44b16bfde94156fd6d34ab9402fa9d847f1ddca4
Chris Gaudet <chrisgaudet9@gmail.com>
2024-08-26 19:09:40 -0500

    completed Inheritance exercise 1

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -16,9 +16,32 @@ namespace Inheritance
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
+            var Teal = new Bird();
+            Teal.WingColor = "Teal";
+            Teal.CanFly = true;
+            Teal.WillMigrate = true;
+            Teal.BeakLength = 2.5;
+
             // Create a class Reptile
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
+
+            var Gecko = new Reptile();
+            Gecko.IsColdBlooded = true;
+            Gecko.IsScaly = true;
+            Gecko.Habitat = "Woods";
+            Gecko.CanGrowTail = true;
+
+            var myAnimals = new Animal[] { Teal, Gecko };
+
+            foreach (var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has {animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
 
 
 
